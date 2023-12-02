a0fc31334c418a4aef5d40da67cbeeda80ffa8d2
76500833 <145300587+76500833@users.noreply.github.com>
2023-12-01 20:40:16 -0500

    btn added
    and bg image

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,22 @@
+dayjs.extend(window.dayjs_plugin_duration);
 window.onload = function() {
-    var timeEl = document.createElement('p')
-    document.body.appendChild(timeEl)
+    let containerForTimer = document.body.getElementsByClassName('hero-text')[0]
+    let timerOnPage = document.body.getElementsByClassName('timer')[0]
     var time = 0;
+    
     function countdown() {
         var timer = setInterval(function() {
-            time++
-            timeEl.textContent = time;
+            time++ 
+            // credit to gihub ai sidebar 
+            var duration = dayjs.duration(time, 'seconds');
+            var formattedTime = `${duration.days()}d ${duration.hours()}h ${duration.minutes()}m ${duration.seconds()}s`;
+            
+            timerOnPage.textContent = formattedTime;
 
         }, 1000);
     }
+let btn = document.getElementById('btn')
+btn.addEventListener('click', function(){
     countdown()
+})
 }
