ac5c0ba991306ca8a89ff42a41bb2eb3398d827e
76500833 <145300587+76500833@users.noreply.github.com>
2023-12-17 19:49:03 -0500

    working

--- assets/script.js
+++ assets/script.js
@@ -1,22 +1,12 @@
-dayjs.extend(window.dayjs_plugin_duration);
-window.onload = function() {
-    let containerForTimer = document.body.getElementsByClassName('hero-text')[0]
-    let timerOnPage = document.body.getElementsByClassName('timer')[0]
-    var time = 0;
-    
-    function countdown() {
-        var timer = setInterval(function() {
-            time++ 
-            // credit to gihub ai sidebar 
-            var duration = dayjs.duration(time, 'seconds');
-            var formattedTime = `${duration.days()}d ${duration.hours()}h ${duration.minutes()}m ${duration.seconds()}s`;
-            
-            timerOnPage.textContent = formattedTime;
+let time = localStorage.getItem("timeFasted") ? parseInt(localStorage.getItem("timeFasted")) : 0;
 
-        }, 1000);
-    }
-let btn = document.getElementById('btn')
-btn.addEventListener('click', function(){
-    countdown()
-})
-}
+let timer = $("<p>").attr("id", "timerP");
+$("body").append(timer);
+
+setInterval (function(){
+    time++;
+    let duration = dayjs.duration(time, 'seconds');
+    let formattedTime = `${duration.hours()}h ${duration.minutes()}m ${duration.seconds()}s`;
+    localStorage.setItem("timeFasted", time);
+    timer.text(formattedTime);
+}, 1000);
